b745caa38737425d9fe1bf5fd5ff6659ed769f65
Agata Grzelewska <agata.grzelewska@elpassion.pl>
2019-08-28 16:29:59 +0200

    test_when_folder_contains_gemfile_it_counts_gems

--- test/gems_counter_test.rb
+++ test/gems_counter_test.rb
@@ -11,15 +11,33 @@ class GemsCounterTest < Minitest::Test
       assert Counter.new(dir).count == []
     end
   end
+
+  def test_when_folder_contains_gemfile_it_counts_gems
+    Dir.mktmpdir do |dir|
+      File.open("#{dir}/Gemfile", "w") {|file| file.write "gem 'rails'"}
+      assert_equal [{ name: 'rails', count: 1 }], Counter.new(dir).count
+    end
+  end
 end
 
 class InvalidPathError < StandardError; end
 
 class Counter
-  def initialize dir
+  def initialize path
+    @path = path
   end
 
   def count
-    []
+    counter = []
+
+    Object.send(:define_method, "gem") do |name|
+      counter = [{name: name, count: 1}]
+    end
+
+    Dir.entries(@path).select {|file| File.file? file }.each do |file|
+      load("#{@path}/#{file}")
+    end
+
+    counter
   end
 end
